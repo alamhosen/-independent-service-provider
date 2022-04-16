0e6d03c515411ce1ab3280674ad69dc343ab8bfa
Alam Hosen <alamhosen.web@gmail.com>
2022-04-16 23:14:43 +0600

    banner complete by carousel

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -20,7 +20,7 @@ const Banner = () => {
                             alt="First slide"
                         />
                         <Carousel.Caption>
-                            <h3>First slide label</h3>
+                            <h3>Wedding Photography</h3>
                             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -32,7 +32,7 @@ const Banner = () => {
                         />
 
                         <Carousel.Caption>
-                            <h3>Second slide label</h3>
+                            <h3>Portrait Photography</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -44,7 +44,7 @@ const Banner = () => {
                         />
 
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
+                            <h3>Engagement Photography</h3>
                             <p>
                                 Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                             </p>
