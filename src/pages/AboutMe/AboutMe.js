598c0b21863e9f575910451de593abf8023c2e6f
Alam Hosen <alamhosen.web@gmail.com>
2022-04-18 17:31:42 +0600

    about me page added

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <div>
+        <div className='container'>
             <h2 className='text-center text-primary my-3'>About Me</h2>
+            <p className='w-75 mx-auto'>Hello, I'm Alam Hosen. I'm a student of Programming Hero". I'm learning web development. I already finished the intermediate level of HTML, CSS, bootstrap, tailwind, javascript, and react.js. Within the next 6 months, I will complete the rest of my course and I want to join a software company. I'm sacrificing many things to achive my goal.</p>
         </div>
     );
 };
