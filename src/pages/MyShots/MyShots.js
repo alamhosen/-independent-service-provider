6f5423f660a3857450d6718f57bc4392e8a0e161
Alam Hosen <alamhosen.web@gmail.com>
2022-04-18 23:26:17 +0600

    Footer updated

--- src/pages/MyShots/MyShots.js
+++ src/pages/MyShots/MyShots.js
@@ -16,39 +16,39 @@ const MyShots = () => {
                     <Col className='col-lg-4 col-md-6 col-sm-12 col-12 mb-4 '>
                         <img
                             src={img1}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt=""
                         />
 
                         <img
                             src={img2}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt="Wintry Mountain Landscape"
                         />
                     </Col>
                     <Col className='col-lg-4 col-md-6 col-sm-12 col-12 mb-4 '>
                         <img
                             src={img3}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt="Mountains in the Clouds"
                         />
 
                         <img
                             src={img4}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt="Boat on Calm Water"
                         />
                     </Col>
                     <Col className='col-lg-4 col-md-6 col-sm-12 col-12 mb-4 '>
                         <img
                             src={img5}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt="Waves at Sea"
                         />
 
                         <img
                             src={img6}
-                            class="w-100 shadow-1-strong rounded mb-4"
+                            className="w-100 shadow-1-strong rounded mb-4"
                             alt="Yosemite National Park"
                         />
                     </Col>
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import facebook from '../../../images/social icons/facebook.png'
+import twitter from '../../../images/social icons/twitter.png'
+import whatsapp from '../../../images/social icons/whatsapp.png'
+import instragram from '../../../images/social icons/instagram.png'
 
 const Footer = () => {
     const today =  new Date();
     const year = today.getFullYear();
     return (
         <footer className='text-center mt-4'>
+            <p className='mb-3 fs-5'>Get me in touch</p>
+            <div className='mb-3'>
+                <a href="https://facebook.com" target='_blank'><img className='mx-1' src={facebook}  alt="" /></a>
+                <a href="https://twitter.com/" target='_blank'><img className='mx-1' src={twitter} alt="" /></a>
+                <a href="https://www.whatsapp.com/" target='_blank'><img className='mx-1' src={whatsapp} alt="" /></a>
+                <a href="https://www.instagram.com/?hl=en" target='_blank'><img className='mx-1' src={instragram} alt="" /></a>
+                
+            </div>
              <p>Copyright © {year} | Photo Galleria | All rights reserved</p>
         </footer>
     );
